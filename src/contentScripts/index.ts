9aedb64be828b9c55828ab992f0c13dcd14a59fa
gogoend <gogoend@qq.com>
2024-07-27 19:41:06 +0800

    feat: 完善插件脚本执行后，在当前标签页body上加其他属性的逻辑

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,3 +1,13 @@
-import('./alive-detect')
-import('./a')
-import('./b')
+import { mittBus } from './utils/mittBus'
+
+const csHaveRunFlag = document.body.hasAttribute('data-gogoend-injected')
+
+if (!csHaveRunFlag) {
+  document.body.setAttribute('data-gogoend-injected', '')
+  mittBus.on('extension-background-destroyed', () => {
+    document.body.removeAttribute('data-gogoend-injected')
+  })
+  import('./alive-detect')
+  import('./a')
+  import('./b')
+}
