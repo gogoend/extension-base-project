d5c3edcc5b4806091c5bf468136426de992abd53
gogoend <gogoend@qq.com>
2024-08-18 17:56:28 +0800

    chore(跟组件 对话框): 处理同一对话框可能会被多次挂载的问题

--- src/utils/mount-el-dialog-as-app.ts
+++ src/utils/mount-el-dialog-as-app.ts
@@ -13,88 +13,100 @@ type ElWithDisposer = HTMLElement & {
   }
 }
 
+const pendingPromise: Record<string, Promise<any>> = {}
+
 export default function mountElDialogAsApp<T extends Component>(Comp: T, props, uniqueElId: string) {
   // #region 对话框组件定义
-  const show = async () => {
+  const show = () => {
+    if (pendingPromise[uniqueElId] instanceof Promise)
+      return pendingPromise[uniqueElId]
+
     const existEl = document.documentElement.querySelector(`#${uniqueElId}`) as ElWithDisposer | null
     if (existEl) {
-      return {
+      return Promise.resolve({
         close: existEl.__close__,
         promise: existEl.__waitee__?.promise,
         kill: existEl.__kill__,
-      }
+      })
     }
-    const shadow = await getShadow()
+    const p = getShadow().then((shadow) => {
+      delete pendingPromise[uniqueElId]
+      const root = shadow.root
+      const container = shadow.container as ElWithDisposer
+      container.id = uniqueElId
 
-    const root = shadow.root
-    const container = shadow.container as ElWithDisposer
-    container.id = uniqueElId
+      let app: ReturnType<typeof createApp> | null = null
+      const kill = () => {
+        props.modelValue = false
+        app?.unmount()
+        container.remove()
+        delete container.__close__
+        container.__waitee__?.reject?.()
+        delete container.__waitee__
+        delete container.__kill__
+        delete props.resolvers
+        app = null
+        mittBus.off('extension-background-destroyed', kill)
+      }
+      mittBus.on('extension-background-destroyed', kill)
+      if (typeof props.onClosed === 'function') {
+        const originOnClosed = props.onClosed
+        props.onClosed = () => {
+          originOnClosed()
+          kill()
+        }
+      }
+      else {
+        props.onClosed = kill
+      }
 
-    let app: ReturnType<typeof createApp> | null = null
-    const kill = () => {
+      const close = async () => {
+        await nextTick() // 假设直接 show().close() ，即改变了对话框在挂载之前的状态，将导致对话框DOM在body上存在，但永远也打不开对话框
+        props.modelValue = false // 设置false，而不是直接卸载组件、删掉元素，保证对话框过渡动画能够播放结束，然后再由onClosed卸载组件、删掉元素
+      }
+
+      container.__close__ = close
+      container.__kill__ = kill
+      container.__waitee__ = {}
+      container.__waitee__.promise = new Promise((resolve, reject) => {
+        container!.__waitee__!.resolve = resolve
+        container!.__waitee__!.reject = reject
+
+        props.resolvers = {
+          resolve,
+          reject,
+        }
+      })
+      container.__waitee__.promise.finally(() => {
+        close()
+      })
+
       props.modelValue = false
-      app?.unmount()
-      container.remove()
-      delete container.__close__
-      container.__waitee__?.reject?.()
-      delete container.__waitee__
-      delete container.__kill__
-      delete props.resolvers
-      app = null
-      mittBus.off('extension-background-destroyed', kill)
-    }
-    mittBus.on('extension-background-destroyed', kill)
-    if (typeof props.onClosed === 'function') {
-      const originOnClosed = props.onClosed
-      props.onClosed = () => {
-        originOnClosed()
-        kill()
+      props['onUpdate:modelValue'] = (v: boolean) => {
+        props.modelValue = v
       }
-    }
-    else {
-      props.onClosed = kill
-    }
+      app = createApp({
+        render: () => h(Comp, props),
+      })
 
-    const close = async () => {
-      await nextTick() // 假设直接 show().close() ，即改变了对话框在挂载之前的状态，将导致对话框DOM在body上存在，但永远也打不开对话框
-      props.modelValue = false // 设置false，而不是直接卸载组件、删掉元素，保证对话框过渡动画能够播放结束，然后再由onClosed卸载组件、删掉元素
-    }
+      const el = document.createElement('div')
+      app.mount(el)
+      root.appendChild(el)
 
-    container.__close__ = close
-    container.__kill__ = kill
-    container.__waitee__ = {}
-    container.__waitee__.promise = new Promise((resolve, reject) => {
-      container!.__waitee__!.resolve = resolve
-      container!.__waitee__!.reject = reject
+      props.modelValue = true
 
-      props.resolvers = {
-        resolve,
-        reject,
+      return {
+        close,
+        kill,
+        promise: container.__waitee__.promise,
       }
-    })
-    container.__waitee__.promise.finally(() => {
-      close()
-    })
-
-    props.modelValue = false
-    props['onUpdate:modelValue'] = (v: boolean) => {
-      props.modelValue = v
-    }
-    app = createApp({
-      render: () => h(Comp, props),
+    }, (err) => {
+      delete pendingPromise[uniqueElId]
+      throw err
     })
 
-    const el = document.createElement('div')
-    app.mount(el)
-    root.appendChild(el)
-
-    props.modelValue = true
-
-    return {
-      close,
-      kill,
-      promise: container.__waitee__.promise,
-    }
+    pendingPromise[uniqueElId] = p
+    return p
   }
   // #endregion
   return {
