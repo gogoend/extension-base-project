15fb2d522efa4c552a0cfb376dc854297fcb3796
gogoend <gogoend@qq.com>
2024-09-08 00:09:11 +0800

    feat(options popup sidepanel): 引入ElementUI

--- src/options/main.ts
+++ src/options/main.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './Options.vue'
 import { setupApp } from '~/logic/common-setup'
 import '../styles'
+import 'element-ui/lib/theme-chalk/index.css'
 
 setupApp(Vue)
+Vue.use(ElementUI)
 const AppCtor = Vue.extend(App)
 new Vue(AppCtor).$mount('#app')
