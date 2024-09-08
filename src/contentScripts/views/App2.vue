08eafe98a531dba144a30b57e192d08d833ac796
gogoend <gogoend@qq.com>
2024-09-09 01:33:29 +0800

    fix(csui 百度边栏): 加入AI 输入框、回答展示

--- src/contentScripts/views/App2.vue
+++ src/contentScripts/views/App2.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Button as ElButton, Option as ElOption, Select as ElSelect } from 'element-ui'
+import { Button as ElButton, Input as ElInput, Option as ElOption, Select as ElSelect } from 'element-ui'
 import { onMessage, sendMessage } from 'webext-bridge/content-script'
 import { v4 as uuid } from 'uuid'
 import mountElDialogAsApp from '../../utils/mount-el-dialog-as-app'
@@ -41,28 +41,77 @@ async function handleCloseClick() {
 }
 
 const selectValue = ref(1)
-onMounted(async () => {
-  const sessionId = await sendMessage(WorkerRequestAiSessionId.tag, new WorkerRequestAiSessionId())
+
+const prompt = ref('')
+const aiResponse = ref('')
+const askLoading = ref(false)
+const hasError = ref(false)
+async function askAi() {
+  if (askLoading.value) {
+    currentInstance.proxy.$message({
+      type: 'warning',
+      message: 'AI正在回答哦',
+    })
+    return
+  }
+  if (!prompt.value.trim()) {
+    currentInstance.proxy.$message({
+      type: 'warning',
+      message: '请输入文本~',
+    })
+    return
+  }
+
+  askLoading.value = true
+  hasError.value = false
+
+  let sessionId
+  try {
+    sessionId = await sendMessage(WorkerRequestAiSessionId.tag, new WorkerRequestAiSessionId())
+  }
+  catch {
+    askLoading.value = false
+    return
+  }
 
   sendMessage(WorkerRequestStreamAi.tag, new WorkerRequestStreamAi({
     connectId: uuid(),
     sessionId,
-    prompt: 'go go go',
+    prompt: prompt.value,
   }))
 
   onMessage(WorkerResponseStreamAi.tag, (message) => {
-    console.warn(message.data.payload)
+    if (message.data.payload.index === 0)
+      aiResponse.value = ''
+
+    aiResponse.value += message.data.payload.text
+
+    if (message.data.payload.index === -1) {
+      askLoading.value = false
+      if (message.data.payload)
+        hasError.value = true
+    }
   })
-})
+}
 </script>
 
 <template>
   <div class="search-engine-insertion">
-    <ElButton @click="handleCloseClick">
+    <ElButton type="text" @click="handleCloseClick">
       关闭
     </ElButton>
     <br>
-    {{ r }}
+    <form @submit.prevent="askAi">
+      <ElInput v-model="prompt" type="textarea" />
+      <ElButton type="primary" :loading="askLoading" @click="askAi">
+        问Ai
+      </ElButton>
+    </form>
+    <div class="ai-content">
+      {{ aiResponse }}<template v-if="hasError">
+        &nbsp;&nbsp;<i class="el-icon-error color-red" />
+      </template>
+    </div>
     <div v-dompurify-html="r" />
     <ElSelect v-model="selectValue" :teleported="false">
       <ElOption :value="1">
