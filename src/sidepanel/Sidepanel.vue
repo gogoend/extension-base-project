818e7f14990619a5eb7c2c30a8c194398434835c
gogoend <gogoend@qq.com>
2024-09-16 14:13:40 +0800

    chore(content script): 删除无用内容

--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -3,13 +3,11 @@ import CloseConfirm from '../contentScripts/views/components/CloseConfirm.vue'
 import mountElDialogAsApp from '../utils/mount-el-dialog-as-app'
 import ChatPanel from '~/components/ChatPanel/index.vue'
 import { handleMessageFactory } from '~/utils/messaging'
-import { storageDemo } from '~/logic/storage'
 import { SidepanelUpdateContextByPageContent } from '~/type/worker-message'
 
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
 }
-const visible = ref(false)
 
 function handleImperativeDialog() {
   mountElDialogAsApp(CloseConfirm, { uniqueElId: 'close-confirm-dialog' })
@@ -31,29 +29,19 @@ onUnmounted(cancelPageContentUpdateListen)
 
 <template>
   <main class="px-4 py-5 text-gray-700">
-    <Logo />
     <div>Sidepanel</div>
-    <SharedSubtitle />
-    <img src="../assets/google-doodle.png">
     <button class="btn mt-2" @click="openOptionsPage">
       Open Options
     </button>
-    <div class="mt-2">
-      <span class="opacity-50">Storage:</span> {{ storageDemo }}
-    </div>
     <div v-if="pageAbstract">
       <div>{{ pageAbstract?.title }}</div>
       <div>{{ pageAbstract?.content }}</div>
     </div>
     <div class="mt-2">
-      <ElButton @click="visible = true">
-        声明式对话框测试
-      </ElButton>
       <ElButton @click="handleImperativeDialog">
         命令式对话框测试
       </ElButton>
     </div>
-    <CloseConfirm :visible.sync="visible" />
 
     <ChatPanel />
   </main>
