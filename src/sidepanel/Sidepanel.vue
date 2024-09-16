d15078a112157b0bef076ee882d96fd4660422f1
gogoend <gogoend@qq.com>
2024-09-17 02:21:12 +0800

    feat(sidepanel): 使用ElTabPane套一层，便于切换各功能

--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+import {
+  TabPane as ElTabPane,
+  Tabs as ElTabs,
+} from 'element-ui'
 import ChatPanel from '~/components/ChatPanel/index.vue'
 </script>
 
 <template>
   <main class="main px-4 py-5 text-gray-700">
-    <ChatPanel class="main__chat-panel-wrap" />
+    <ElTabs type="card">
+      <ElTabPane label="聊天">
+        <ChatPanel class="main__chat-panel-wrap" />
+      </ElTabPane>
+      <ElTabPane label="翻译">
+        Hello
+      </ElTabPane>
+    </ElTabs>
   </main>
 </template>
 
@@ -18,6 +29,18 @@ main.main {
   .main__chat-panel-wrap {
     flex: 1 1 auto;
     overflow: auto;
+  }
+  &::v-deep(.el-tabs) {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+  }
+  &::v-deep(.el-tabs__content) {
+    height: 100%;
+  }
+  &::v-deep(.el-tab-pane) {
+    height: 100%;
   }
 }
 </style>
