85b749f0076d277bf82ca31b3dc501ae3acf1539
gogoend <gogoend@qq.com>
2024-09-16 09:33:58 +0800

    feat(messaging): 完善handleMessageFactory类型定义（各种类型运算……）；完善sendToXX类型定义

--- src/utils/messaging.ts
+++ src/utils/messaging.ts
@@ -10,21 +10,21 @@ export function sendToBackground<T extends (WorkerMessage.WorkerBaseMessage)>(me
   })
 }
 
-export function sendToPopup(message) {
+export function sendToPopup<T extends (WorkerMessage.WorkerBaseMessage)>(message: T): Promise<MessagingResponseTypeMap[T['messageType']]> {
   return browser.runtime.sendMessage({
     target: 'popup',
     message,
   })
 }
 
-export function sendToSidepanel(message) {
+export function sendToSidepanel<T extends (WorkerMessage.WorkerBaseMessage)>(message: T): Promise<MessagingResponseTypeMap[T['messageType']]> {
   return browser.runtime.sendMessage({
     target: 'sidepanel',
     message,
   })
 }
 
-export async function sendToOffscreen(message) {
+export async function sendToOffscreen<T extends (WorkerMessage.WorkerBaseMessage)>(message: T): Promise<MessagingResponseTypeMap[T['messageType']]> {
   await sendToBackground(new WorkerMessage.EnsureOffscreen())
   return browser.runtime.sendMessage({
     target: 'offscreen',
@@ -32,29 +32,60 @@ export async function sendToOffscreen(message) {
   })
 }
 
-export function sendToTabById(tabId: number, message) {
+export function sendToTabById<T extends (WorkerMessage.WorkerBaseMessage)>(tabId: number, message: T): Promise<MessagingResponseTypeMap[T['messageType']]> {
   return browser.tabs.sendMessage(tabId, { message, target: 'tab' })
 }
 
-export function broadcastToTabs(tabIdList: number[], message) {
+export function broadcastToTabs<T extends (WorkerMessage.WorkerBaseMessage)>(tabIdList: number[], message: T) {
   return Promise.allSettled(tabIdList.map(tabId => sendToTabById(tabId, message)))
 }
 
-export async function broadcastToAllTabs(message) {
+export async function broadcastToAllTabs<T extends (WorkerMessage.WorkerBaseMessage)>(message: T) {
   const tabIdList = await browser.tabs.query({}) ?? []
   return broadcastToTabs(tabIdList.map(it => it.id).filter(it => typeof it === 'number'), message)
 }
 
+type TargetContext =
+  | 'background'
+  | 'popup'
+  | 'sidepanel'
+  | 'offscreen'
+  | 'tab'
+
+// #region 以各消息messageType属性为key，映射各消息
+// 1. 提取 WorkerMessage 命名空间下所有类的类型并组合成联合类型
+type WorkerMessageUnion = typeof WorkerMessage[keyof typeof WorkerMessage]
+// 2. 引入的类型比较混杂，且引入的是按值推断的类型（引入的类型构造函数，而不是接口），因此按照能否构造过滤出所有构造函数
+type OnlyWithConstructors<T> = T extends new (...args: any[]) => any ? T : never
+type MessagesWithConstructors = OnlyWithConstructors<WorkerMessageUnion>
+// 3. 取一下构造函数实例的类型
+type MessagesInstanceType = InstanceType<MessagesWithConstructors>
+// 4. 过滤出基于WorkerBaseMessage的类型，并把这些类型组合为联合类型
+type FilteredMessages<T> = T extends WorkerMessage.WorkerBaseMessage ? T : never
+type WorkMessageTypeUnion = FilteredMessages<MessagesInstanceType>
+// 5. 创建出映射类型，key为messageType属性为key，value为各自对应的消息
+type MessageTypeMap = {
+  [M in WorkMessageTypeUnion as M['messageType']]: M
+}
+// #endregion
+
 export function handleMessageFactory(
-  targetContext:
-    | 'background'
-    | 'popup'
-    | 'sidepanel'
-    | 'offscreen'
-    | 'tab',
+  targetContext: TargetContext,
 ) {
-  return function handleMessage(tag, handler) {
-    const innerHandler = (message, sender: Browser.Runtime.MessageSender) => {
+  return function handleMessage<T extends keyof MessagingResponseTypeMap>(
+    tag: T,
+    handler: (
+      message: {
+        message: MessageTypeMap[T]
+        target: TargetContext
+      },
+      sender: Browser.Runtime.MessageSender
+    ) => any,
+  ) {
+    const innerHandler = (message: {
+      message: MessageTypeMap[T]
+      target: TargetContext
+    }, sender: Browser.Runtime.MessageSender) => {
       if (
         message.target !== targetContext
         || message.message.messageType !== tag
@@ -68,11 +99,11 @@ export function handleMessageFactory(
   }
 }
 
-export function getConnectName(tag, id) {
+export function getConnectName(tag: string, id: string) {
   return [tag, id].join('@') as `${string}@${string}`
 }
 export function parseConnectName(name: string) {
-  return name.split('@')
+  return name.split('@') as [string, string]
 }
 export function sendToStreamResponsePort(message, { resolvePredict, rejectPredict, streamHandler } = {}) {
   const port = browser.runtime.connect({ name: getConnectName(message.messageType, uuid()) })
